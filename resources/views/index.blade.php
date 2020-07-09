05094ff5539507a7d837ba94c52e13846a97b15a
voktyrp <maxim.prytkov@bk.ru>
2020-07-09 15:43:46 +0400

    init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>larareact</title>
+    </head>
+    <body>
+        <h1>POMOGITE</h1>
+    </body>
+</html>
